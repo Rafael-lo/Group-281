4bb2679395bdb2c774bb87cab19bd41762b0ac53
mehebbetfz <me.farzaliyev@gmail.com>
2025-09-17 21:56:36 +0400

    lesson_6

---
+++ Js/lesson_6/index.js
@@ -0,0 +1,68 @@
+// ## 🧮 Объекты и массивы
+// - Создание объектов
+
+let a = 8
+const b = 78
+//   0        1         2
+let fruits = ['mango', 'apple', 'banana']
+
+// key: value
+
+// let user = {
+// 	name: "Alex",
+// 	surname: "Alexeyev",
+// 	age: 4,
+// 	hasCar: true
+// };
+
+// console.log(user);
+
+// user.surname = "Muradov";
+// delete user.name;
+
+// console.log(user);
+
+// console.log("Name: " + user.name);
+// console.log("Age: " + user.age);
+// console.log("Car: " + user.hasCar);
+
+// - Свойства и методы
+
+let txt = '   Hello,world! 56'
+
+// Методы
+// console.log(txt.toUpperCase());
+// console.log(txt.includes("Hello"));
+// console.log(txt.concat("  Bro"));
+// console.log(txt.endsWith("56"));
+// console.log(txt.indexOf("H")); // 3
+// console.log(txt.startsWith("   H")); // true
+// console.log(txt.repeat(5));
+// console.log(txt.replace("Hello", "Bonjor"));
+// console.log(txt.slice(10));
+// console.log(txt.split(",")); //
+// console.log(txt.toLowerCase());
+// console.log(txt.trim()); // trimStart, trimEnd
+
+// Свойство
+// console.log(txt.length); //
+
+// - Массивы и методы массивов
+
+// const cars = ["mersedes", "bmw", "tesla", "audi"];
+
+// cars.push("xiaomi");
+// cars.pop();
+// cars.shift();
+// cars.unshift("xiaomi");
+// console.log(cars.join("#"));
+// console.log(cars.concat(["xiaomi, samsung"]));
+// console.log(cars.indexOf("bmw"));
+// const newCars = cars.slice();
+// console.log(newCars);
+// console.log(cars);
+
+// console.log(cars.length); // 4
+
+//TODO - Spread/Rest операторы
+//TODO - JSON (parse, stringify)
